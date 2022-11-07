e67043df39da19c0982fec50a4bbacd105a42bd0
Mike Solomon <mike120982@gmail.com>
2022-11-07 11:27:14 +0200

    Fixes vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-const path = require("path");
-
+import * as path from "path";
 export default {
-	resolve: {
-		alias: {
-			PureScript: process.env.PROD_EXPERIMENTAL ? path.resolve(__dirname, 'output-es/Main/') : path.resolve(__dirname, 'output/Main/'),
-		},
-	}
+    resolve: {
+        alias: {
+            PureScript: process.env.NODE_ENV === 'production'
+              ? path.resolve(__dirname, "output-es/Main/")
+              : path.resolve(__dirname, "output/Main/"),
+          },
+    }
 }
